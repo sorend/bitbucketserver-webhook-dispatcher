b0868304d5b555cc5325555a0245633d29e14f9e
Soren A D <sad@bankdata.dk>
2020-06-03 09:47:23 +0200

    added request to eventcontext

--- src/main/java/com/github/sorend/bitbucketserver/webhook/context/EventContext.java
+++ src/main/java/com/github/sorend/bitbucketserver/webhook/context/EventContext.java
@@ -1,7 +1,9 @@
 package com.github.sorend.bitbucketserver.webhook.context;
 
+import io.helidon.webserver.ServerRequest;
 import com.cdancy.bitbucket.rest.BitbucketApi;
 
 public interface EventContext {
     BitbucketApi api();
+    ServerRequest request();
 }
